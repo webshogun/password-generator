3900c06874c7ec6a5c73a095caf7e3f3ddee3621
Bohdan Barulin <webshogun@proton.me>
2023-08-02 23:23:13 +0300

    Wrote a function that generates password

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const stylesheet = document.getElementById('theme');
 let selectedTheme = localStorage.getItem('theme');
+const pass = document.getElementById('password');
 
 function setTheme(theme) {
   stylesheet.setAttribute('href', `styles/${theme}-theme.css`);
@@ -16,4 +17,17 @@ function themeHandler() {
 function copyHandler(id) {
   const copy = document.getElementById(id);
   navigator.clipboard.writeText(copy.value);
+}
+
+function generatePassword() {
+  const length = 12;
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  pass.setAttribute('value', password);
 }
